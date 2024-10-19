00396a66f07d049256ad36d500b5c9243fbb4629
Mijux <mijux-dev@proton.me>
2024-10-19 20:17:55 +0000

    fix promox api

--- api/proxmox.py
+++ api/proxmox.py
@@ -8,7 +8,7 @@ from wakeonlan import send_magic_packet
 
 class ProxmoxAPI:
 
-    HOST = f"https://{getenv('HOST_PROXMOX','pve.local')}"
+    HOST = f"https://{getenv('HOST_PROXMOX','pve.local')}:8006"
     HOST_MAC_ADDR = getenv("HOST_PROXMOX_MAC_ADDR", None)
     TOKEN_ID = getenv("API_PROXMOX_TOKEN_ID", None)
     TOKEN_SECRET = getenv("API_PROXMOX_TOKEN_SECRET", None)
@@ -18,13 +18,17 @@ class ProxmoxAPI:
         command = "command=shutdown"
         headers = {
             "Content-Type": "application/x-www-form-urlencoded",
-            "Authorization": f"{ProxmoxAPI.TOKEN_ID}={ProxmoxAPI.TOKEN_SECRET}",
+            "Authorization": f"PVEAPIToken={ProxmoxAPI.TOKEN_ID}={ProxmoxAPI.TOKEN_SECRET}",
         }
 
-        endpoint = "/api2/json/nodes/rattler/status"
+        endpoint = "api2/json/nodes/rattler/status"
         url = join(ProxmoxAPI.HOST, endpoint)
 
-        req = rget(url, headers=headers, data=command, verify=False)
+        verify_ssl = False
+        if getenv("HOST_PROXMOX_SSL_VERIFY", "True") in ["true", "y", "1", "yes"]:
+            verify_ssl = True
+
+        req = rget(url, verify=verify_ssl, headers=headers, data=command)
 
         if req.status_code == 200:
             return True
