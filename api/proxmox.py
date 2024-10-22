7d9fd7514d3d5935674d4df725bf64111b8cc7e6
Mijux <mijux-dev@proton.me>
2024-10-22 17:11:45 +0200

    feat: add ping check to proxmox
    when power on the proxmox server with to WOL function, it is impossible to get any response. I had a sleep of 60 seconds then call the ping that try to know if the server is up. The ping is compatible with windows and *nix

--- api/proxmox.py
+++ api/proxmox.py
@@ -2,7 +2,10 @@
 
 from os import getenv
 from os.path import join
+from platform import system as get_system_name
 from requests import post as rpost
+from subprocess import call as subcall
+from time import sleep
 from wakeonlan import send_magic_packet
 
 from utils.logger import get_logger
@@ -10,7 +13,8 @@ from utils.logger import get_logger
 
 class ProxmoxAPI:
 
-    HOST = f"https://{getenv('HOST_PROXMOX','pve.local')}:8006"
+    HOST = getenv("HOST_PROXMOX", "pve.local")
+    HOST_BASE_URL = f"https://{HOST}:8006"
     HOST_MAC_ADDR = getenv("HOST_PROXMOX_MAC_ADDR", None)
     TOKEN_ID = getenv("API_PROXMOX_TOKEN_ID", None)
     TOKEN_SECRET = getenv("API_PROXMOX_TOKEN_SECRET", None)
@@ -24,7 +28,7 @@ class ProxmoxAPI:
         }
 
         endpoint = "api2/json/nodes/rattler/status"
-        url = join(ProxmoxAPI.HOST, endpoint)
+        url = join(ProxmoxAPI.HOST_BASE_URL, endpoint)
 
         verify_ssl = False
         if getenv("HOST_PROXMOX_SSL_VERIFY", None) in ["true", "y", "1", "yes"]:
@@ -43,3 +47,18 @@ class ProxmoxAPI:
     def power_on() -> bool:
         get_logger().info("Try to wake up server and wait")
         send_magic_packet(ProxmoxAPI.HOST_MAC_ADDR)
+        sleep(60)
+
+        # Windows takes -n as argument and Linux takes -c
+        param = "-n" if get_system_name().lower() == "windows" else "-c"
+
+        command = ["ping", param, "1", ProxmoxAPI.HOST]
+
+        if subcall(command) == 0:
+            get_logger().info("Server has been power on successfully")
+            return True
+
+        get_logger().error(
+            "Can't check if server is powered on - pls check your WOL config or increase timeout"
+        )
+        return False
