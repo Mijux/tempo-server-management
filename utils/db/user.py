1abd7936403c50d54d884a944e8c8ee167abdf97
Mijux <mijux-dev@proton.me>
2024-10-23 18:44:54 +0000

    feat: add some db functions
    add add_user and remove_user functions

---
+++ utils/db/user.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import Session
+
+from models.user import User
+from utils.dbconn import get_session
+
+from utils.exceptions import DBUserAlreadyExistsError, DBUserDoesNotExistError
+
+
+def add_user(user: dict) -> bool:
+    with get_session() as db_session:
+        new_user: User = User(
+            id=user.get("id"),
+            username=user.get("username"),
+            avatar=user.get("avatar"),
+            permission_level=0,
+            arrival_date=user.get("arrival_date"),
+        )
+
+        db_session.add(new_user)
+
+        try:
+            db_session.commit()
+        except IntegrityError:
+            db_session.rollback()
+            raise DBUserAlreadyExistsError(user.get("id"), user.get("username"))
+
+
+def remove_user(user_id: str) -> bool:
+    with get_session() as db_session:
+        user_to_remove: User = db_session.query(User).filter(User.id == user_id).first()
+
+        if user_to_remove:
+            db_session.delete(user_to_remove)
+            db_session.commit()
+        else:
+            raise DBUserDoesNotExistError(user_id)
--- utils/exceptions.py
+++ utils/exceptions.py
@@ -14,4 +14,20 @@ class DeployException(Exception):
 
 
 class ConfigurationException(Exception):
-    error_code = 500
+    pass
+
+
+class DBUserAlreadyExistsError(Exception):
+
+    def __init__(self, id, username):
+        self.id = id
+        self.username = username
+        self.err_msg = f"The user {self.username}({self.id}) already exists"
+
+        super().__init__(self.err_msg)
+
+
+class DBUserDoesNotExistError(Exception):
+    def __init__(self, id):
+        self.id = id
+        self.err_msg = f"The user with id {self.id} does not exist"
